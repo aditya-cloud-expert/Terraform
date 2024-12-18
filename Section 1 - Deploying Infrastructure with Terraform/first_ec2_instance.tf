08c58ee668b724ecc488967ef6172a001715228d
Aditya Vishal Joshi <aditya.cloud.expert@gmail.com>
2024-12-18 22:03:30 +0530

    Create Fist Instance

---
+++ Section 1 - Deploying Infrastructure with Terraform/first_ec2_instance.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.81.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "us-west-2"
+  access_key = "my-access-key"
+  secret_key = "my-secret-key"
+}
+
+resource "aws_instance" "sample-resource" {
+  ami                     = "ami-0dcc1e21636832c5d"
+  instance_type           = "m5.large"
+}
